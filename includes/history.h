233b6c19a226aeded1ea004fc218797b33308ff8
Bartlomiej JANIK <bjanik@e2r8p2.42.fr>
2018-01-16 16:48:00 +0100

    add search_history

--- includes/history.h
+++ includes/history.h
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/15 14:58:40 by bjanik            #+#    #+#             */
-/*   Updated: 2018/01/16 11:44:24 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/01/16 16:37:55 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,8 @@ int				display_history(t_history history);
 int				add_cmd_to_history(t_history *history, char *const command);
 int				remove_cmd_from_history(t_history *history, const int offset);
 int				clear_all_history(t_history *history);
+char			*search_in_history_by_num(t_history *history, const int number);
+char			*search_in_history_by_str(t_history *history,
+										const char *const str);
 
 #endif
---
+++ srcs/search_in_history.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_str_in_history.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/16 14:59:31 by bjanik            #+#    #+#             */
+/*   Updated: 2018/01/16 16:39:45 by bjanik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "history.h"
+
+char	*search_in_history_by_num(t_history *history, const int number)
+{
+	if (ft_abs(number) > history->len)
+		return (NULL);
+	if (number < 0)
+		return (history->history[len - number]);
+	return (history->history[number]);
+}
+
+char	*search_in_history_by_str(t_history *history, const char *const str)
+{
+	size_t	i;
+	size_t	len_str;
+
+	if (!str)
+		return (NULL);
+	len_str = ft_strlen(str);
+	i = 0;
+	while (history->history[i])
+	{
+		if (!ft_strncmp(history->history[i], str, len_str))
+			return (history->history[i]);
+		i++;
+	}
+	return (NULL);
+}
