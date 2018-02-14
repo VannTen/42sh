c72117f39275d567bb4a338941e70fb7042edd84
Bartlomiej JANIK <bjanik@e1r1p12.42.fr>
2018-02-14 16:17:43 +0100

    delete upper bound of reallocation

--- srcs/main/realloc_str.c
+++ srcs/main/realloc_str.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/09 16:54:48 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/10 15:00:50 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/02/13 18:04:24 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ int	realloc_str(t_string *str)
 	char	*tmp;
 
 	tmp = str->str;
-	if (str->size * 2 > 1000000000
-	|| !(str->str = ft_strnew(str->size * 2)))
+	if (!(str->str = ft_strnew(str->size * 2)))
 	{
-		tmp = NULL;
-		return (-1);
+		ft_strdel(&tmp);
+		return (MALLOC_FAIL);
 	}
 	str->size *= 2;
 	ft_strcpy(str->str, tmp);
