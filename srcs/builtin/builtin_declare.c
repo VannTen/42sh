fc89df646abcd9162f603dd95dc414d69acba48b
Bartlomiej JANIK <bjanik@e2r3p10.42.fr>
2018-02-19 16:29:56 +0100

    add builtins export && declare

---
+++ srcs/builtin/builtin_declare.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_declare.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/19 14:58:54 by bjanik            #+#    #+#             */
+/*   Updated: 2018/02/19 16:24:52 by bjanik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+int	builtin_declare(char **argv, t_env *env)
+{
+	if (!argv || !argv[0])
+		ft_dprintf(STDERR_FILENO, "42sh: declare: invalid parameters\n");
+	display_local_global_env(env->env_list);
+	return (0);
+}
