f12f59e1a36a6c0f96edf7ebdd7d533d0fd5464a
Bartlomiej JANIK <bjanik@e2r3p10.42.fr>
2018-02-19 16:55:15 +0100

    modif declare builtin

--- srcs/builtin/builtin_declare.c
+++ srcs/builtin/builtin_declare.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 14:58:54 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/19 16:24:52 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/02/19 16:43:08 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 int	builtin_declare(char **argv, t_env *env)
 {
 	if (!argv || !argv[0])
+	{
 		ft_dprintf(STDERR_FILENO, "42sh: declare: invalid parameters\n");
+		return (1);
+	}
 	display_local_global_env(env->env_list);
 	return (0);
 }
