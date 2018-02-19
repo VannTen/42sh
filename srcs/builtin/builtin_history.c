292500d3b7f2f82fc6c790e15a55530dc2640beb
Bartlomiej JANIK <bjanik@e2r3p10.42.fr>
2018-02-19 16:55:36 +0100

    builtin history

--- srcs/builtin/builtin_history.c
+++ srcs/builtin/builtin_history.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 12:53:10 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/15 17:22:30 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/02/19 14:54:34 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,43 @@
 static int	history_usage(const char opt, const int usage_reason)
 {
 	if (usage_reason == NUMERIC_ARG_REQUIRED)
-		ft_dprintf(STDERR_FILENO, "bsh: history: option requires an argument\n");
+		ft_dprintf(STDERR_FILENO, "42sh: history: option requires an argument\n");
 	if (usage_reason == INVALID_OPT)
-		ft_dprintf(STDERR_FILENO, "bsh: history: -%c: invalid option\n", opt);
+		ft_dprintf(STDERR_FILENO, "42sh: history: -%c: invalid option\n", opt);
 	ft_dprintf(STDERR_FILENO, "history: usage: history [-c] [-d offset] [n]\n");
 	return (1);
 }
 
-static int	num_is_eligible_to_be_an_entry(const char *num)
+static int	num_is_eligible_to_be_an_entry(char *num, t_history *history)
 {
-	if ((num[0] == '+' && ft_strisdigit(num + 1)) || ft_strisdigit(num))
+	int	position;
+
+	(num[0] == '+') ? num++ : 0;
+	if (ft_strisdigit(num))
 	{
-		if (ft_strlen(num + (num[0] == '+')) > ft_strlen(INT_MAX_STR))
+		if (ft_strlen(num) > ft_strlen(INT_MAX_STR)
+				|| ft_strcmp(num, INT_MAX_STR) >= 0)
 			return (0);
-	if (ft_strcmp(num, INT_MAX_STR) >= 0)
+	}
+	else
+		ft_dprintf(STDERR_FILENO, "42sh: history: -d: history position out of"
+		"range\n");
+	return (1);
 }
 
 static int	history_del_one_entry(t_history *history, const char *num)
 {
 	if (num)
 	{
-		if (num[0] == '+' ||
+		if (num_is_eligible_to_be_an_entry(num) == 1)
+			delete_cmd_from_history(history, ft_atoi(num));
+		else
+			ft_dprintf(STDERR_FILENO, "42sh: history: -d: history position out"
+						"of range\n");
 	}
 	else
-	{
-		ft_dprintf(STDERR_FILENO, "bsh: history: -d: option requires an "
-								"argument");
-	}
+		ft_dprintf(STDERR_FILENO, "42sh: history: -d: option requires an "
+					"argument");
 	return (0);
 }
 
@@ -62,7 +72,8 @@ int	builtin_history(char **args, t_env *env)
 	{
 		if (!args[i][1])
 		{
-			ft_putendl_fd("bsh: history: `-': not a valid identifier", STDERR);
+			ft_putendl_fd("bsh: history: `-': not a valid identifier",
+					STDERR_FILENO);
 				return (1);
 		}
 		if ((wrong_opt = check_arg_opt(args[i] + 1, HISTORY_OPTIONS, options)))
@@ -70,7 +81,7 @@ int	builtin_history(char **args, t_env *env)
 	}
 	if (ft_strchr(options, 'c'))
 		clear_all_history(history);
-	if (ft_strchr(options, 'd'))
+	else if (ft_strchr(options, 'd'))
 		history_del_one_entry(history, args[i]);
 	(i == 1) ? display_history(history) : 0;
 	return (0);
