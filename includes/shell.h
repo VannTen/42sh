f931a336d0d6dbc83fe210fba497df1b84db8b8b
Bartlomiej JANIK <bjanik@e2r5p14.42.fr>
2018-02-24 19:06:44 +0100

    add exit code macros + prototypes

--- includes/shell.h
+++ includes/shell.h
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/21 12:31:57 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/23 16:31:14 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/02/24 17:52:22 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@
 # define EVENT_NOT_FOUND 1
 # define MALLOC_FAIL -1
 # define INPUT_TOO_LONG -2
+# define UNMATCHED_QUOTES 2
+# define PERMISSION_DENIED 126
+# define IS_DIRECTORY 126
+# define NO_SUCH_FILE_DIRECTORY 127
 
 typedef struct	s_bsh
 {
@@ -44,6 +48,13 @@ typedef struct	s_bsh
 int				init_termcaps(t_bsh *bsh);
 t_bsh			*get_shell_data(void);
 t_bsh			*shell_init(char **environ, int argc, char **argv);
+int				getline(t_input *input, const int interactive,
+							const int mode, size_t *state);
 int				readline_process(t_input *input, t_lexer *lexer,
 								t_history *history, const int interactive);
+
+void			sh_exit_message(const char *error_message);
+void			sh_no_such_file_or_directory(const char *file);
+void			sh_permission_denied(const char *file);
+void			sh_is_a_directory(const char *file);
 #endif
