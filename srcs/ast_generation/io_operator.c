0ac94f7618274108220c56c6c0d6356e67cd6848
ble-berr <ble-berr@student.42.fr>
2018-02-26 18:43:31 +0100

    Fixed leak

--- srcs/ast_generation/io_operator.c
+++ srcs/ast_generation/io_operator.c
@@ -6,7 +6,7 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/09 16:08:25 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/02/14 10:07:06 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/02/26 15:46:53 by ble-berr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	*create_io_operator(void const *lex_value)
 	struct s_container	*container;
 
 	(void)lex_value;
-	container = new_container(NULL, NULL, 0, e_ast_container_label_io_operator);
+	container = new_container(NULL, &destroy_container, 0, e_ast_container_label_io_operator);
 	return (container);
 }
 
