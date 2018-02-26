205feb10998a3a89fb1734949374c012d40e8299
ble-berr <ble-berr@student.42.fr>
2018-02-26 18:44:45 +0100

    Fixed return value verification looking for ERROR instead of SUCCESS

--- srcs/execution/io_redir_aggregate.c
+++ srcs/execution/io_redir_aggregate.c
@@ -6,7 +6,7 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 13:03:03 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/02/24 08:45:20 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/02/26 15:59:18 by ble-berr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	io_redir_aggregate(struct s_sh_io_redirect const *io_redir,
 	if (io_redir && fd_backups_loc
 			&& (io_number = str_to_fd(io_redir->ionum)) != -1
 			&& (target_fd = str_to_fd(io_redir->target)) != -1
-			&& backup_filedescriptor(io_number, fd_backups_loc)
+			&& !backup_filedescriptor(io_number, fd_backups_loc)
 			&& !saved_filedescriptor_conflict(target_fd, *fd_backups_loc))
 	{
 		if (dup2(target_fd, io_number) != -1)
