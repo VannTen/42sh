c939d421351d31958fbde19767240dca34a8bb52
Bartlomiej JANIK <bjanik@e2r4p13.42.fr>
2018-02-27 17:43:12 +0100

    add pass_tokens_to_parser.c file

---
+++ srcs/main/pass_tokens_to_parser.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pass_tokens_to_parser.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/27 15:27:43 by bjanik            #+#    #+#             */
+/*   Updated: 2018/02/27 15:41:41 by bjanik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "shell.h"
+
+size_t	get_tok_id(void const *token)
+{
+	t_token const *tok;
+
+	if (token == g_tokens_name)
+		return (BANG + 1);
+	tok = token;
+	return (tok->type);
+}
+
+void	*take_token(void *token_list_address)
+{
+	t_token	**list_address;
+	void	*token;
+
+	list_address = token_list_address;
+	if (*list_address != NULL)
+	{
+		token = *list_address;
+		*list_address = (*list_address)->next;
+		return (token);
+	}
+	else
+		return ((void*)g_tokens_name);
+}
+
