97ec8b930a955251d8b8dc14c6b091b08353d870
Bartlomiej JANIK <bjanik@e2r4p13.42.fr>
2018-02-27 17:41:54 +0100

    update shell_getenv prototype

--- includes/env.h
+++ includes/env.h
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 15:50:58 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/27 12:34:45 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/02/27 14:26:23 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ typedef struct			s_env
 int						init_env(t_env *env, char **environ);
 t_env_list				*ft_getenv(t_env_list *env_list,
 						const char *const name);
-char					*shell_getenv(t_env *env, char *name);
+char					*shell_getenv(t_env *env, char const *const name);
 
 char					**convert_env_to_array(t_env_list *env_list,
 						const size_t len);
