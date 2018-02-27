b2519e718c4b113c865d4572fa426e44ae5df6e3
Bartlomiej JANIK <bjanik@e1r2p22.42.fr>
2018-02-27 11:40:42 +0100

    add next field in t_lsit def...

--- includes/libbj.h
+++ includes/libbj.h
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/27 10:55:18 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/27 11:04:11 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/02/27 11:40:05 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
-typedef struct	s_list
+typedef struct		s_list
 {
-	void		*content;
-	size_t		content_size;
-}				t_list;
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
 
-int				ft_abs(int i);
-int				ft_str_isdigit(char *str);
-void			ft_free_string_tab(char ***tab);
-void			ft_swap(void **a, void **b);
-char			*ft_strnjoin(char *s1, int n, ...);
-char			*ft_strndup(const char *s1, size_t n);
-t_list			*ft_lstnew(void const *content, size_t content_size);
-void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+int					ft_abs(int i);
+int					ft_str_isdigit(char *str);
+void				ft_free_string_tab(char ***tab);
+void				ft_swap(void **a, void **b);
+char				*ft_strnjoin(char *s1, int n, ...);
+char				*ft_strndup(const char *s1, size_t n);
+t_list				*ft_lstnew(void const *content, size_t content_size);
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 #endif
