cb9342130e2a12bdd2e1e48a8281b0f4f724ac51
Bartlomiej JANIK <bjanik@e1r4p16.42.fr>
2018-02-28 14:53:25 +0100

    modif init_expander prototype

--- srcs/expansion/init_expander.c
+++ srcs/expansion/init_expander.c
@@ -6,25 +6,21 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 12:43:04 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/28 13:19:32 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/02/28 14:37:31 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-t_expander	*init_expander(t_env *env)
+int		init_expander(t_expander *exp, t_env *env)
 {
-	t_expander *exp;
-
-	if ((exp = (t_expander*)malloc(sizeof(t_expander))) == NULL)
-		return (NULL);
 	exp->state = INIT_EXP;
 	exp->event = START_EXP;
 	exp->env = env->env_list;
 	exp->buffer_size = INITIAL_SIZE;
 	if (!(exp->buffer = ft_strnew(exp->buffer_size)))
-		return (NULL);
+		return (MALLOC_FAIL);
 	exp->buffer_len = 0;
 	exp->tmp = NULL;
-	return (exp);
+	return (0);
 }
