332085594660adce51695cd94d816516441169a9
Bartlomiej JANIK <bjanik@e1r4p16.42.fr>
2018-02-28 14:31:09 +0100

    strnew size not size + 1

--- srcs/expansion/init_expander.c
+++ srcs/expansion/init_expander.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 12:43:04 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/27 13:07:55 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/02/28 13:19:32 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_expander	*init_expander(t_env *env)
 	exp->event = START_EXP;
 	exp->env = env->env_list;
 	exp->buffer_size = INITIAL_SIZE;
-	if (!(exp->buffer = ft_strnew(exp->buffer_size + 1)))
+	if (!(exp->buffer = ft_strnew(exp->buffer_size)))
 		return (NULL);
 	exp->buffer_len = 0;
 	exp->tmp = NULL;
