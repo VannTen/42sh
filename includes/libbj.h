0b155edc1910984ec7d2d275cae7146b979f68f1
Bart <bart@macbook-pro-de-bart.home>
2018-03-01 00:47:17 +0100

    updated libbj header

--- includes/libbj.h
+++ includes/libbj.h
@@ -12,8 +12,27 @@
 
 #ifndef LIBBJ_H
 # define LIBBJ_H
+# include "libft.h"
 # include <stdlib.h>
 # include <stdarg.h>
+
+# define TEMP_SIZE 4096
+# define FD_MAX 7168
+
+typedef struct		s_fd
+{
+	char			*tmp;
+	char			*nl;
+	size_t			mem_size;
+}					t_fd;
+
+typedef struct		s_dlst
+{
+	void			*data;
+	size_t			data_size;
+	struct s_dlst	*next;
+	struct s_dlst	*prev;
+}					t_dlst;
 
 typedef struct		s_list
 {
@@ -27,7 +46,11 @@ int					ft_str_isdigit(char *str);
 void				ft_free_string_tab(char ***tab);
 void				ft_swap(void **a, void **b);
 char				*ft_strnjoin(char *s1, int n, ...);
+void				ft_dlstadd(t_dlst **alst, t_dlst *new);
+t_dlst				*ft_dlstnew(void const *data, size_t data_size);
+void				ft_dlstdel(t_dlst **alst, void (*del)(void *, size_t));
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 size_t				ft_tab_len(char **tab);
+int					get_next_line(int fd, char **line);
 #endif
