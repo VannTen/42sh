3323f1b5b391f1a8e6a9340f542872f6959315d6
ble-berr <ble-berr@student.42.fr>
2018-02-28 15:47:43 +0100

    'included env.h for builtins'

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,12 +6,14 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/24 06:29:55 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/02/24 08:10:56 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/02/28 15:30:50 by ble-berr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
+
+# include "env.h"
 
 typedef int	(*t_builtin)(char **argv, t_env *env);
 
@@ -21,15 +23,16 @@ struct		s_sh_builtin
 	t_builtin	ft;
 };
 
-# define BUILTIN_COUNT 6
-
-extern struct s_sh_builtin const	g_builtin_array[BUILTIN_COUNT];
-
 int	builtin_cd(char **argv, t_env *env);
 int	builtin_echo(char **argv, t_env *env);
 int	builtin_exit(char **argv, t_env *env);
 int	builtin_env(char **argv, t_env *env);
 int	builtin_setenv(char **argv, t_env *env);
 int	builtin_unsetenv(char **argv, t_env *env);
+int	builtin_hash(char **argv, t_env *env);
+
+# define BUILTIN_COUNT 7
+
+extern struct s_sh_builtin const	g_builtin_array[BUILTIN_COUNT];
 
 #endif
