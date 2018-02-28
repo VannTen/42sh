102a4f4be4eab1dc9874c35f771537291ccb9010
ble-berr <ble-berr@student.42.fr>
2018-02-28 09:14:09 +0100

    Now properly sets exit value from shell data if no argument is given

--- srcs/builtin/builtin_exit.c
+++ srcs/builtin/builtin_exit.c
@@ -6,13 +6,14 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/26 19:02:54 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/02/25 22:36:39 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/02/28 09:12:23 by ble-berr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "execution.h"
 #include "shell_errmsg.h"
+#include "shell.h"
 
 static void	exit_badarg(char *arg)
 {
@@ -36,6 +37,14 @@ static int	exit_arg_check(char **args)
 	return (0);
 }
 
+static int	get_shell_exit_status(void)
+{
+	t_bsh	*shell_status;
+
+	shell_status = get_shell_data();
+	return (shell_status ? shell_status->exit_status : 42);
+};
+
 int			builtin_exit(char **args, t_env *env)
 {
 	unsigned char	exit_value;
@@ -44,7 +53,7 @@ int			builtin_exit(char **args, t_env *env)
 	if (exit_arg_check(args))
 		return (-1);
 	if (args[1] == NULL)
-		exit_value = 42;
+		exit_value = get_shell_exit_status();
 	else if (string_is_digit(args[1]))
 		exit_value = (unsigned char)ft_atoi(args[1]);
 	else
