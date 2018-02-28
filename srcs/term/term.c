d345eb1b1f7d27340a0ef30c42a6ecb4973cc98c
Bartlomiej JANIK <bjanik@e1r4p16.42.fr>
2018-02-28 15:23:27 +0100

    proper handling of init termcaps failure

--- srcs/term/term.c
+++ srcs/term/term.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/05 14:55:29 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/27 13:15:49 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/02/28 15:19:43 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,42 @@
 int		restore_custom_attr(t_term *term)
 {
 	if (tcsetattr(STDIN_FILENO, TCSANOW, &term->custom_attr) == -1)
-		ft_dprintf(STDERR_FILENO, "Unable to set custom attributes");
+		ft_dprintf(STDERR_FILENO, "42sh: Unable to set custom attributes");
 	return (0);
 }
 
 int		init_termcaps(t_bsh *bsh)
 {
 	char	*termtype;
+	int		ret;
 
+	ret = 0;
 	if (!(termtype = ft_getenv(bsh->env.env_list, "TERM")->value))
-		ft_dprintf(STDERR_FILENO, "Missing $TERM variable");
-	if (tgetent(NULL, termtype) < 1)
-		ft_dprintf(STDERR_FILENO, "tgetent failed\n");
-	if (tcgetattr(STDIN_FILENO, &bsh->term.initial_attr) == -1)
-		ft_dprintf(STDERR_FILENO, "Unable to get terminal attributes");
-	if (tcgetattr(STDIN_FILENO, &bsh->term.custom_attr) == -1)
-		ft_dprintf(STDERR_FILENO, "Unable to get terminal attributes");
-	bsh->term.custom_attr.c_lflag &= ~(ICANON | ECHO);
-	bsh->term.custom_attr.c_cc[VMIN] = 1;
-	bsh->term.custom_attr.c_cc[VTIME] = 0;
-	if (tcsetattr(STDIN_FILENO, TCSANOW, &bsh->term.custom_attr) == -1)
-		ft_dprintf(STDERR_FILENO, "Unable to set custom attributes");
+		ret = ft_dprintf(STDERR_FILENO, "Missing TERM variable");
+	else if (tgetent(NULL, termtype) < 1)
+		ret = ft_dprintf(STDERR_FILENO, "42sh: Tgetent failed\n");
+	else if (tcgetattr(STDIN_FILENO, &bsh->term.initial_attr) == -1)
+		ret = ft_dprintf(STDERR_FILENO, "42sh: Unable to get term attributes");
+	else if (tcgetattr(STDIN_FILENO, &bsh->term.custom_attr) == -1)
+		ret = ft_dprintf(STDERR_FILENO, "42sh: Unable to get term attributes");
+	else
+	{
+		bsh->term.custom_attr.c_lflag &= ~(ICANON | ECHO);
+		bsh->term.custom_attr.c_cc[VMIN] = 1;
+		bsh->term.custom_attr.c_cc[VTIME] = 0;
+		if (tcsetattr(STDIN_FILENO, TCSANOW, &bsh->term.custom_attr) == -1)
+			ret = ft_dprintf(STDERR_FILENO, "42sh: Unable to set custom"
+					"attributes\n");
+	}
+	if (ret)
+		exit(EXIT_FAILURE);
 	return (0);
 }
 
 int		restore_initial_attr(t_term *term)
 {
 	if (tcsetattr(STDIN_FILENO, TCSANOW, &term->initial_attr) == -1)
-		ft_dprintf(STDERR_FILENO, "Unable to reset initial attributes");
+		ft_dprintf(STDERR_FILENO, "42sh: Unable to reset initial attributes");
 	return (0);
 }
 
