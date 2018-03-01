830379d670454e88bc7914ea5d0b6a28e501fa49
Bartlomiej JANIK <bjanik@e1r4p16.42.fr>
2018-03-01 18:01:19 +0100

    add builtin history

--- srcs/builtin/builtin_history.c
+++ srcs/builtin/builtin_history.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 12:53:10 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/19 14:54:34 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/01 16:09:46 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,32 +26,32 @@ static int	history_usage(const char opt, const int usage_reason)
 	return (1);
 }
 
-static int	num_is_eligible_to_be_an_entry(char *num, t_history *history)
+static int	num_is_eligible_to_be_an_entry(char *num)
 {
-	int	position;
-
 	(num[0] == '+') ? num++ : 0;
-	if (ft_strisdigit(num))
+	if (ft_str_isdigit(num))
 	{
-		if (ft_strlen(num) > ft_strlen(INT_MAX_STR)
-				|| ft_strcmp(num, INT_MAX_STR) >= 0)
+		if (ft_strlen(num) > 10
+				|| (ft_strlen(num) == 10 && ft_strcmp(num, INT_MAX_STR)))
 			return (0);
 	}
 	else
 		ft_dprintf(STDERR_FILENO, "42sh: history: -d: history position out of"
-		"range\n");
+		" range\n");
 	return (1);
 }
 
-static int	history_del_one_entry(t_history *history, const char *num)
+static int	history_del_one_entry(t_history *history, char *num)
 {
+	int	n;
+
+	n = 0;
 	if (num)
 	{
-		if (num_is_eligible_to_be_an_entry(num) == 1)
-			delete_cmd_from_history(history, ft_atoi(num));
-		else
+		if (!num_is_eligible_to_be_an_entry(num) || (n =ft_atoi(num)))
+			if (remove_cmd_from_history(history, n))
 			ft_dprintf(STDERR_FILENO, "42sh: history: -d: history position out"
-						"of range\n");
+						" of range\n");
 	}
 	else
 		ft_dprintf(STDERR_FILENO, "42sh: history: -d: option requires an "
@@ -62,21 +62,21 @@ static int	history_del_one_entry(t_history *history, const char *num)
 int	builtin_history(char **args, t_env *env)
 {
 	t_history	*history;
-	char const	history_options[] = "cd";
 	char		wrong_opt;
 	char		options[3];
+	int			i;
 
+	(void)env;
 	history = &get_shell_data()->history;
 	i = 0;
 	while (args[++i] && args[i][0] == '-' && ft_strcmp("--", args[i]))
 	{
 		if (!args[i][1])
 		{
-			ft_putendl_fd("bsh: history: `-': not a valid identifier",
-					STDERR_FILENO);
+			ft_putendl_fd("bsh: history: `-': not a valid identifier", 2);
 				return (1);
 		}
-		if ((wrong_opt = check_arg_opt(args[i] + 1, HISTORY_OPTIONS, options)))
+		if ((wrong_opt = check_arg_opt(args[i] + 1, "cd", options)))
 			return (history_usage(wrong_opt, INVALID_OPT));
 	}
 	if (ft_strchr(options, 'c'))
