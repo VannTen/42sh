f59598f2cb15142afaa36dfd0d638a0e478f4349
Bartlomiej JANIK <bjanik@e1r4p16.42.fr>
2018-03-01 16:09:32 +0100

    trunc history file before saving

--- srcs/history/save_history_to_histfile.c
+++ srcs/history/save_history_to_histfile.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/16 11:39:40 by bjanik            #+#    #+#             */
-/*   Updated: 2018/02/28 15:36:37 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/01 15:05:39 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	save_history_to_histfile(t_history *history)
 	int	fd;
 
 	i = -1;
-	if ((fd = open(history->file, O_CREAT | O_WRONLY, 0644)) < 0)
+	if ((fd = open(history->file, O_CREAT | O_TRUNC | O_WRONLY, 0644)) < 0)
 		return (1);
 	while (++i < history->len)
 	{
