a22848a4e289a911a26e75be98bdbebdbe778a37
Bartlomiej JANIK <bjanik@e1r4p16.42.fr>
2018-03-02 20:25:57 +0100

    fix history search when sigint

--- srcs/main/readline_sigint.c
+++ srcs/main/readline_sigint.c
@@ -6,7 +6,7 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 16:52:25 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/03/02 19:08:15 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/02 19:59:08 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ void	readline_sigint(int sigid)
 	(void)sigid;
 	g_sigint_detected = 1;
 	bsh = get_shell_data();
+	if (bsh->input.type == HISTORY_SEARCH)
+		go_to_beg_line(&bsh->input);
 	reset_buffer(&bsh->input);
 	tputs(tgetstr("me", NULL), 1, putchar_termcaps);
 	if (bsh->input.buf_tmp)
 		ft_memset(bsh->input.buf_tmp, 0, ft_strlen(bsh->input.buf_tmp));
-	if (bsh->input.type == HISTORY_SEARCH)
-		go_to_beg_line(&bsh->input);
 	bsh->input.pivot = -1;
 	bsh->input.state = STANDARD;
 	bsh->input.type = REGULAR_INPUT;
