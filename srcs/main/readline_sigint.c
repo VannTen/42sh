c2002b5067c65cac754113cae85bfda979a779e3
Bartlomiej JANIK <bjanik@e1r4p16.42.fr>
2018-03-02 19:22:39 +0100

    handling signal with g_sigint_detected

--- srcs/main/readline_sigint.c
+++ srcs/main/readline_sigint.c
@@ -6,17 +6,20 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 16:52:25 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/03/01 16:52:26 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/03/02 19:08:15 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+int		g_sigint_detected = 0;
 
 void	readline_sigint(int sigid)
 {
 	t_bsh	*bsh;
 
 	(void)sigid;
+	g_sigint_detected = 1;
 	bsh = get_shell_data();
 	reset_buffer(&bsh->input);
 	tputs(tgetstr("me", NULL), 1, putchar_termcaps);
@@ -24,12 +27,9 @@ void	readline_sigint(int sigid)
 		ft_memset(bsh->input.buf_tmp, 0, ft_strlen(bsh->input.buf_tmp));
 	if (bsh->input.type == HISTORY_SEARCH)
 		go_to_beg_line(&bsh->input);
-	if (bsh->input.type != HISTORY_SEARCH)
-		write(STDIN_FILENO, "\n", 1);
 	bsh->input.pivot = -1;
 	bsh->input.state = STANDARD;
 	bsh->input.type = REGULAR_INPUT;
 	bsh->history.current = bsh->history.len;
 	reset_lexer(&bsh->lexer);
-	print_prompt(&bsh->input, BOLD_CYAN);
 }
