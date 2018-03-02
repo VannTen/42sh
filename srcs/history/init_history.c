fe65bfadfc740fed93d3e830d68ef07471ab6276
Bartlomiej JANIK <bjanik@e1r4p16.42.fr>
2018-03-02 16:07:09 +0100

    proper handling if malloc fails

--- srcs/history/init_history.c
+++ srcs/history/init_history.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/21 12:39:42 by bjanik            #+#    #+#             */
-/*   Updated: 2018/01/24 13:14:49 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/02 13:21:34 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,20 @@ static char	*get_home_dir(void)
 
 int			init_history(t_history *history, int size)
 {
+	char	*home_dir;
+
 	if (size < 0 || size > DEFAULT_HISTSIZE)
 		size = DEFAULT_HISTSIZE;
 	if (!(history->history = (char**)malloc(sizeof(char*) * (size + 1))))
 		return (MALLOC_FAIL);
 	ft_memset(history->history, 0, (size + 1) * sizeof(char*));
-	if (!(history->file = ft_strnjoin(get_home_dir(), 2, "/", ".42sh_history")))
+	if (!(home_dir = get_home_dir()))
 		return (MALLOC_FAIL);
+	if (!(history->file = ft_strnjoin(home_dir, 2, "/", ".42sh_history")))
+	{
+		ft_strdel(&home_dir);
+		return (MALLOC_FAIL);
+	}
 	history->current = 0;
 	history->len = 0;
 	history->size = size;
