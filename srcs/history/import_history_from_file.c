af331f991b6a2a768cd0bec20f3b3f7f4dad2dec
Bartlomiej JANIK <bjanik@e1r4p16.42.fr>
2018-03-02 16:06:48 +0100

    proper handling if malloc fails

--- srcs/history/import_history_from_file.c
+++ srcs/history/import_history_from_file.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/15 16:36:42 by bjanik            #+#    #+#             */
-/*   Updated: 2018/01/25 17:49:03 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/02 13:54:27 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,19 @@ int			import_history_from_file(t_history *history)
 	int		fd;
 
 	if ((fd = open(history->file, O_CREAT | O_RDONLY | O_APPEND, 0644)) < 0)
-		return (2);
+	{
+		ft_dprintf(STDERR_FILENO, "42sh: opening of history file failed\n");
+		return (1);
+	}
 	while (get_next_line(fd, &line) > 0)
 	{
-		add_cmd_to_history(history, line);
+		if (add_cmd_to_history(history, line) == MALLOC_FAIL)
+		{
+			clear_all_history(history);
+			ft_strdel(&line);
+			ft_dprintf(STDERR_FILENO, "42sh: history import failed\n");
+			break ;
+		}
 		ft_strdel(&line);
 	}
 	close(fd);
