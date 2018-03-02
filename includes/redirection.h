86be06391be74c3fa68f82ed65bc1be353f5d2a2
ble-berr <ble-berr@student.42.fr>
2018-03-02 13:21:17 +0100

    Fixed "-" as target in aggregate redirections

--- includes/redirection.h
+++ includes/redirection.h
@@ -6,7 +6,7 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 12:02:11 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/02/24 08:53:01 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/03/02 14:29:03 by ble-berr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	io_redir_here(struct s_sh_io_redirect const *io_redirect,
 		t_lst **fd_backups_loc);
 
 int	backup_filedescriptor(int fd, t_lst **fd_backups_loc);
-int	saved_filedescriptor_conflict(int fd, t_lst *fd_backups);
+int	filedescriptor_is_a_backup(int fd, t_lst *fd_backups);
 
 int	str_to_fd(char const *str);
 
