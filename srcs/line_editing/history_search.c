8041e805e339612c4a9df0d95f4277c78a3d5ba0
Bartlomiej JANIK <bjanik@e2r5p11.42.fr>
2018-03-03 15:03:31 +0100

    all good

--- srcs/line_editing/history_search.c
+++ srcs/line_editing/history_search.c
@@ -6,11 +6,17 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 14:19:38 by bjanik            #+#    #+#             */
-/*   Updated: 2018/03/02 19:14:47 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/03 13:51:32 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+static void	exit_history_search_mode(t_input *input)
+{
+	go_to_beg_line(input);
+	print_prompt(input, BOLD_CYAN);
+}
 
 static int	init_search(t_input *input)
 {
@@ -23,8 +29,7 @@ static int	init_search(t_input *input)
 	input->term->first_line_len = input->term->width - 16;
 	if ((ret = (wait_for_input(input, HISTORY_SEARCH) == UNEXPECTED_EOF)))
 	{
-		go_to_beg_line(input);
-		print_prompt(input, BOLD_CYAN);
+		exit_history_search_mode(input);
 		exit_ctrl_d(input);
 	}
 	else if (ret == CATCH_SIGINT)
@@ -46,8 +51,7 @@ int			handle_history_search(t_input *input)
 		{
 			if (ft_strstr(input->history->history[i], input->buffer))
 			{
-				go_to_beg_line(input);
-				print_prompt(input, BOLD_CYAN);
+				exit_history_search_mode(input);
 				input->history->current = i;
 				cp_history_to_buffer(input);
 				input->type = REGULAR_INPUT;
@@ -56,8 +60,7 @@ int			handle_history_search(t_input *input)
 			i--;
 		}
 	}
-	go_to_beg_line(input);
-	print_prompt(input, BOLD_CYAN);
+	exit_history_search_mode(input);
 	input->type = REGULAR_INPUT;
 	return (0);
 }
