1516aa86f000f63507ace7c1bceb201873cf6bae
Bartlomiej JANIK <bjanik@e3r4p16.42.fr>
2018-03-05 17:03:07 +0100

    unset returns 0 now not 1

--- srcs/builtin/builtin_unset.c
+++ srcs/builtin/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 15:41:10 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/03/05 15:56:27 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/03/05 16:50:00 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ int	builtin_unset(char **argv, t_env *env)
 			remove_variable_from_env(env, argv[1], LOCAL);
 		}
 	}
-	return (1);
+	return (0);
 }
