2f8100a1a8313bca09b9019a428364fdd50206f8
Bartlomiej JANIK <bjanik@e3r4p16.42.fr>
2018-03-05 15:46:47 +0100

    fixed issue with empty value

--- srcs/builtin/builtin_export.c
+++ srcs/builtin/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 15:12:09 by bjanik            #+#    #+#             */
-/*   Updated: 2018/03/04 18:52:19 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/05 15:39:23 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 #include <unistd.h>
 #include "execution.h"
 
-static int	export_usage(const char opt, const int usage_reason)
+static int	export_usage(const char opt)
 {
-	(void)usage_reason;
 	ft_dprintf(STDERR_FILENO, "42sh: export: -%c: invalid option\n", opt);
 	ft_dprintf(STDERR_FILENO, "export: usage: export [-n] [name [value]] \n");
 	return (1);
@@ -25,15 +24,36 @@ static int	export_usage(const char opt, const int usage_reason)
 static int	export_option_n(t_env *env, char **argv)
 {
 	t_env_list	*var;
+	int			ret;
 
 	if (argv[0])
 	{
 		if ((var = ft_getenv(env->env_list, argv[0])))
 			var->exportable = 0;
 		env->has_changed = TRUE;
+		if (argv[1])
+		{
+			ret = append_variable_to_env(env, argv[0], argv[1], LOCAL);
+			if (!ret && !ft_strcmp("PATH", argv[0]))
+				clear_hashtable();
+		}
 	}
-	append_variable_to_env(env, argv[0], argv[1] ? argv[1] : "",
-			LOCAL);
+	return (0);
+}
+
+static int	set_to_global(t_env *env, char *av1, char *av2)
+{
+	t_env_list	*var;
+
+	if ((var = ft_getenv(env->env_list, av1)))
+	{
+		if (av2)
+			append_variable_to_env(env, av1, av2, GLOBAL);
+		var->exportable = 1;
+	}
+	else
+		append_variable_to_env(env, av1, av2 ? av2 : "", GLOBAL);
+	env->has_changed = TRUE;
 	return (0);
 }
 
@@ -55,12 +75,12 @@ int			builtin_export(char **av, t_env *env)
 			return (1);
 		}
 		if ((wrong_opt = check_arg_opt(av[i] + 1, export_options, options)))
-			return (export_usage(wrong_opt, 0));
+			return (export_usage(wrong_opt));
 	}
 	if (ft_strchr(options, 'n'))
 		return (export_option_n(env, av + i));
 	else if (is_valid_variable_name(av[1]))
-		return (append_variable_to_env(env, av[1], av[2] ? av[2] : "", GLOBAL));
+		return (set_to_global(env, av[1], av[2]));
 	return (ft_dprintf(STDERR_FILENO, "%s: not a valid variable name\n",
 				av[1]));
 }
