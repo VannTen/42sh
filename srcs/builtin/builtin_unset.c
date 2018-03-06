41b39458cee9ee98e6d1ccfe4326613ec4349adf
ble-berr <ble-berr@student.42.fr>
2018-03-06 17:22:03 +0100

    unset now clears hashtable if env unset

--- srcs/builtin/builtin_unset.c
+++ srcs/builtin/builtin_unset.c
@@ -6,11 +6,11 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 15:41:10 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/03/05 16:50:00 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/06 17:21:30 by ble-berr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "env.h"
+#include "execution.h"
 
 int	builtin_unset(char **argv, t_env *env)
 {
@@ -20,6 +20,8 @@ int	builtin_unset(char **argv, t_env *env)
 		{
 			remove_variable_from_env(env, argv[1], GLOBAL);
 			remove_variable_from_env(env, argv[1], LOCAL);
+			if (!ft_strcmp(argv[1], "PATH"))
+				clear_hashtable();
 		}
 	}
 	return (0);
