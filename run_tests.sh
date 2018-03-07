094d34980a1ee202aef63e6435cb9a1608eb62f2
ble-berr <ble-berr@student.42.fr>
2018-03-07 13:27:59 +0100

    Added test script and test dir for dev purposes

---
+++ run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+SHELL="./42sh"
+
+if [ -z "$1" ]
+then
+	echo 'usage: serial_tester.sh [ directory ]'
+	exit 1
+fi
+
+while [ ! -f $SHELL ]
+do
+	read -p './42sh does not exist, please input path to shell:' SHELL
+done
+
+if [ -d "$1" ]
+then
+	for	testfile in "$1"/*.test
+	do
+		echo 'testfile:' $testfile
+		echo 'vvvvvvvvvv Result vvvvvvvvvv'
+		./42sh < $testfile
+		echo ============================
+	done
+elif [ -e "$1" ]
+then
+	echo "$1: Not a directory."
+else
+	echo "$1: Not found."
+fi
