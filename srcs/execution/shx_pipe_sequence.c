ec5cb3d8ab8d6065f9862f5a9b3c372a645fa73d
ble-berr <ble-berr@student.42.fr>
2018-03-07 12:51:05 +0100

    Fixed pipe_buf[0] leaking in child

--- srcs/execution/shx_pipe_sequence.c
+++ srcs/execution/shx_pipe_sequence.c
@@ -6,7 +6,7 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/25 10:08:12 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/03/05 14:44:15 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/03/07 12:45:46 by ble-berr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,20 @@ static t_bool	stdfd(int pipe_buf[2])
 	return (TRUE);
 }
 
-static pid_t	create_child(int pipe_in, int pipe_out,
-		struct s_sh_simple_command *simple_command, t_bool close_pipe_out)
+static pid_t	create_child(int pipe_in, int pipe_buf[2],
+		struct s_sh_simple_command *simple_command, t_bool last_command)
 {
-	pid_t	father;
+	pid_t const	father = fork();
 
-	if (!(father = fork()))
+	if (!father)
 	{
+		close(pipe_buf[0]);
 		dup2(pipe_in, STDIN_FILENO);
 		if (pipe_in != STDIN_FILENO)
 			close(pipe_in);
-		dup2(pipe_out, STDOUT_FILENO);
-		if (pipe_out != STDOUT_FILENO)
-			close(pipe_out);
+		dup2(pipe_buf[1], STDOUT_FILENO);
+		if (pipe_buf[1] != STDOUT_FILENO)
+			close(pipe_buf[1]);
 		if (simple_command)
 			simple_command->child = TRUE;
 		exit(shx_simple_command(simple_command));
@@ -43,8 +44,8 @@ static pid_t	create_child(int pipe_in, int pipe_out,
 	{
 		if (pipe_in != STDIN_FILENO)
 			close(pipe_in);
-		if (close_pipe_out)
-			close(pipe_out);
+		if (!last_command)
+			close(pipe_buf[1]);
 		return (father);
 	}
 }
@@ -58,8 +59,8 @@ static int		spawn_pipe(t_lst *sequence, int pipe_in)
 	next = advance_list(sequence, 1);
 	if (next != NULL ? !pipe(pipe_buf) : stdfd(pipe_buf))
 	{
-		father = create_child(pipe_in, pipe_buf[1],
-				(void*)get_lst_elem(sequence, 0), next != NULL);
+		father = create_child(pipe_in, pipe_buf,
+				(void*)get_lst_elem(sequence, 0), next == NULL);
 		if (0 < father)
 		{
 			if (next != NULL)
@@ -68,7 +69,10 @@ static int		spawn_pipe(t_lst *sequence, int pipe_in)
 			return (0);
 		}
 		else
+		{
+			close(pipe_buf[0]);
 			ft_dprintf(2, "42sh: failed to fork.\n");
+		}
 	}
 	else
 		ft_dprintf(2, "42sh: failed to pipe.\n");
