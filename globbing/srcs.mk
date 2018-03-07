49a84f7c1916495cdc17930b62183420a0863a54
heynard <h.eynard@orange.fr>
2018-03-07 16:07:06 +0100

    compilation globbing

---
+++ globbing/srcs.mk
@@ -0,0 +1,32 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    srcs.mk                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: heynard <heynard@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/03/07 16:00:21 by heynard           #+#    #+#              #
+#    Updated: 2018/03/07 16:03:02 by heynard          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SRCS = bracket/bracket_count.c\
+	   bracket/bracket_final.c\
+	   bracket/bracket_free.c\
+	   bracket/bracket_list_functions.c\
+	   bracket/bracket_move.c\
+	   bracket/bracket_verify.c\
+	   bracket/glob_bracket_func.c\
+	   bracket/glob_bracket.c\
+	   bracket/glob_parse_bracket.c\
+	   candidats/glob_dir.c\
+	   candidats/glob_slash.c\
+	   ft_glob.c\
+	   match/glob_extend.c\
+	   match/glob_match_functions.c\
+	   match/glob_match_square_bracket.c\
+	   match/glob_wildcard.c\
+	   tools/ft_2dstrjoin.c\
+	   tools/ft_free2dstr.c\
+	   tools/ft_strchr_spec.c\
+	   tools/glob_copy.c
