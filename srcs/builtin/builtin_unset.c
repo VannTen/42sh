ac069b8406854a808ed563315a4fbb96fb5656d4
ble-berr <ble-berr@student.42.fr>
2018-03-14 16:35:32 +0100

    unset now only calls remove vraible once

--- srcs/builtin/builtin_unset.c
+++ srcs/builtin/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 15:41:10 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/03/06 17:21:30 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/03/14 16:29:45 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ int	builtin_unset(char **argv, t_env *env)
 	{
 		if (argv[1])
 		{
-			remove_variable_from_env(env, argv[1], GLOBAL);
-			remove_variable_from_env(env, argv[1], LOCAL);
+			remove_variable_from_env(env, argv[1], GLOBAL_AND_LOCAL);
 			if (!ft_strcmp(argv[1], "PATH"))
 				clear_hashtable();
 		}
