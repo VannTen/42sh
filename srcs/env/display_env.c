ff3c3e84e1493db6af791b2685c89aeb3965d7f7
Bartlomiej JANIK <bjanik@e1r5p18.42.fr>
2018-03-20 12:29:50 +0100

    does not display variable if name and/or value is NULL

--- srcs/env/display_env.c
+++ srcs/env/display_env.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 16:00:29 by bjanik            #+#    #+#             */
-/*   Updated: 2018/03/01 12:06:35 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/20 11:54:09 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	display_global_env(t_env_list const *env_list)
 	while (env_list)
 	{
 		if (env_list->exportable == GLOBAL)
-			ft_printf("%s=%s\n", env_list->name, env_list->value);
+			if (env_list->name && env_list->value)
+				ft_printf("%s=%s\n", env_list->name, env_list->value);
 		env_list = env_list->next;
 	}
 	return (0);
@@ -27,9 +28,12 @@ int	display_local_global_env(t_env_list const *env_list)
 {
 	while (env_list)
 	{
-		if (env_list->exportable == GLOBAL)
-			ft_printf("exported ");
-		ft_printf("%s=%s\n", env_list->name, env_list->value);
+		if (env_list->name && env_list->value)
+		{
+			if (env_list->exportable == GLOBAL)
+				ft_printf("exported ");
+			ft_printf("%s=%s\n", env_list->name, env_list->value);
+		}
 		env_list = env_list->next;
 	}
 	return (0);
