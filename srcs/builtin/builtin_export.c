9d6c2d2040fd8189083fdaf32016e534e654b798
Bartlomiej JANIK <bjanik@e1r5p18.42.fr>
2018-03-20 12:30:39 +0100

    correctly updates env_len in every case

--- srcs/builtin/builtin_export.c
+++ srcs/builtin/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 15:12:09 by bjanik            #+#    #+#             */
-/*   Updated: 2018/03/06 17:51:53 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/20 12:14:20 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,19 @@ static int	export_option_n(t_env *env, char **argv)
 
 	if (argv[0] && !is_valid_variable_name(argv[0]))
 		return (ft_dprintf(2, "%s: not a valid variable name\n", argv[0]));
-	if (argv[0])
+	if ((var = ft_getenv(env->env_list, argv[0])))
 	{
-		if ((var = ft_getenv(env->env_list, argv[0])))
-		{
-			var->exportable = 0;
-			env->env_len--;
-		}
-		env->has_changed = TRUE;
-		if (!var || argv[1])
-		{
-			ret = append_variable_to_env(env, argv[0], argv[1] ? argv[1] : "",
-					LOCAL);
-			if (!ret && !ft_strcmp("PATH", argv[0]))
-				clear_hashtable();
-		}
+		(var->exportable == GLOBAL) ? env->env_len-- : 0;
+		var->exportable = LOCAL;
 	}
+	if (!var || argv[1])
+	{
+		ret = append_variable_to_env(env, argv[0], argv[1] ? argv[1] : "",
+				LOCAL);
+		if (!ret && !ft_strcmp("PATH", argv[0]))
+			clear_hashtable();
+	}
+	env->has_changed = TRUE;
 	return (0);
 }
 
@@ -53,13 +50,15 @@ static int	set_to_global(t_env *env, char *av1, char *av2)
 	int			ret;
 
 	ret = 1;
-	if ((var = ft_getenv(env->env_list, av1)))
+	if ((var = ft_getenv(env->env_list, av1)) && var->exportable == LOCAL)
 	{
 		if (av2)
 			ret = append_variable_to_env(env, av1, av2, GLOBAL);
 		else
+		{
 			var->exportable = 1;
-		env->env_len++;
+			env->env_len++;
+		}
 	}
 	else
 		ret = append_variable_to_env(env, av1, av2 ? av2 : "", GLOBAL);
