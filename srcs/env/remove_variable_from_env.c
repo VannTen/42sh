0be4f10119a7c023520fc80db9b671db467c40e2
Bartlomiej JANIK <bjanik@e1r5p18.42.fr>
2018-03-20 12:27:46 +0100

    ok

--- srcs/env/remove_variable_from_env.c
+++ srcs/env/remove_variable_from_env.c
@@ -6,18 +6,18 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 14:25:10 by bjanik            #+#    #+#             */
-/*   Updated: 2018/03/05 15:59:07 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/03/20 11:31:43 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static void	free_env_var(t_env_list *var)
+static void	free_env_var(t_env_list **var)
 {
-	ft_strdel(&var->name);
-	ft_strdel(&var->value);
-	var->next = NULL;
-	ft_memdel((void**)&var);
+	ft_strdel(&(*var)->name);
+	ft_strdel(&(*var)->value);
+	(*var)->next = NULL;
+	ft_memdel((void**)var);
 }
 
 /*
@@ -44,8 +44,8 @@ int			remove_variable_from_env(t_env *env, const char *const name,
 				tmp = tmp->next;
 			tmp->next = remove_var->next;
 		}
-		free_env_var(remove_var);
-		env->env_len--;
+		(remove_var->exportable == GLOBAL) ? env->env_len-- : 0;
+		free_env_var(&remove_var);
 		env->has_changed = TRUE;
 	}
 	return (0);
