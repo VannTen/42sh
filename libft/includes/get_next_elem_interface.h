ab16b88d52c42b41cfb9568c6e59ed9b00c9a9cd
Bartlomiej JANIK <bjanik@e3r7p17.42.fr>
2018-03-21 18:24:01 +0100

    set BUFF_SIZE to 4096 instead of 100

--- libft/includes/get_next_elem_interface.h
+++ libft/includes/get_next_elem_interface.h
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 18:23:03 by mgautier          #+#    #+#             */
-/*   Updated: 2017/10/09 13:32:51 by mgautier         ###   ########.fr       */
+/*   Updated: 2018/03/21 16:33:16 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 # define ONE_LINE_READ 1
 # define READ_ERROR -1
 # define FILE_IS_OVER 0
-# define BUFF_SIZE 100
+# define BUFF_SIZE 4096
 
 /*
 ** Types definitions
