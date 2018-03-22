360d61ca4e97892e340d5d122a63ee1c82aff62e
Bartlomiej JANIK <bjanik@e3r7p17.42.fr>
2018-03-22 15:24:40 +0100

    fix norm

--- srcs/main/readline_sigint.c
+++ srcs/main/readline_sigint.c
@@ -6,7 +6,7 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 16:52:25 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/03/22 13:40:34 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/22 15:19:38 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,5 @@ void	readline_sigint(int sigid)
 	(bsh->input.type != HEREDOC_INPUT) ? reset_lexer(&bsh->lexer) : 0;
 	bsh->input.type = REGULAR_INPUT;
 	bsh->history.current = bsh->history.len;
-	bsh->exit_status= 1;
+	bsh->exit_status = 1;
 }
