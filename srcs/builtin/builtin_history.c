e62a042e24ca652f8ba65389d94db275ef517821
Bartlomiej JANIK <bjanik@e3r7p17.42.fr>
2018-03-22 15:24:03 +0100

    builtin history sets correctly exit status on error

--- srcs/builtin/builtin_history.c
+++ srcs/builtin/builtin_history.c
@@ -6,7 +6,7 @@
 /*   By: bjanik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 12:53:10 by bjanik            #+#    #+#             */
-/*   Updated: 2018/03/21 18:04:04 by bjanik           ###   ########.fr       */
+/*   Updated: 2018/03/22 15:00:54 by bjanik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,9 @@
 #define INVALID_OPT 0
 #define NUMERIC_ARG_REQUIRED 1
 
-static int	history_usage(const char opt, const int usage_reason)
+static int	history_usage(const char opt)
 {
-	if (usage_reason == NUMERIC_ARG_REQUIRED)
-		ft_dprintf(STDERR_FILENO,
-				"42sh: history: option requires an argument\n");
-	if (usage_reason == INVALID_OPT)
-		ft_dprintf(STDERR_FILENO, "42sh: history: -%c: invalid option\n", opt);
+	ft_dprintf(STDERR_FILENO, "42sh: history: -%c: invalid option\n", opt);
 	ft_dprintf(STDERR_FILENO, "history: usage: history [-c] [-d offset] [n]\n");
 	return (1);
 }
@@ -37,8 +33,7 @@ static int	num_is_eligible_to_be_an_entry(char *num)
 			return (0);
 	}
 	else
-		ft_dprintf(STDERR_FILENO, "42sh: history: -d: history position out of"
-		" range\n");
+		return (0);
 	return (1);
 }
 
@@ -51,12 +46,12 @@ static int	history_del_one_entry(t_history *history, char *num)
 	{
 		if (!num_is_eligible_to_be_an_entry(num) || (n = ft_atoi(num)))
 			if (remove_cmd_from_history(history, n))
-				ft_dprintf(STDERR_FILENO, "42sh: history: -d: history position "
-						"out of range\n");
+				return (ft_dprintf(STDERR_FILENO, "42sh: history: -d: history "
+				"position out of range\n"));
 	}
 	else
-		ft_dprintf(STDERR_FILENO, "42sh: history: -d: option requires an "
-					"argument\n");
+		return (ft_dprintf(STDERR_FILENO, "42sh: history: -d: option requires "
+		"an argument\n"));
 	return (0);
 }
 
@@ -78,11 +73,11 @@ int			builtin_history(char **args, t_env *env)
 			return (1);
 		}
 		if ((wrong_opt = check_arg_opt(args[i] + 1, "cd", options)))
-			return (history_usage(wrong_opt, INVALID_OPT));
+			return (history_usage(wrong_opt));
 	}
 	if (ft_strchr(options, 'c'))
 		clear_all_history(history);
 	else if (ft_strchr(options, 'd'))
-		history_del_one_entry(history, args[i]);
+		return (history_del_one_entry(history, args[i]));
 	return (i == 1) ? display_history(history, args[i]) : 0;
 }
