6d2942adce91da1a3d1d860bf11e375e60501b9f
ble-berr <ble-berr@student.42.fr>
2018-03-23 13:50:00 +0100

    unset and unsetenv now go through all arguments

--- srcs/builtin/builtin_unset.c
+++ srcs/builtin/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 15:41:10 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/03/14 16:29:45 by marvin           ###   ########.fr       */
+/*   Updated: 2018/03/23 13:38:59 by ble-berr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int	builtin_unset(char **argv, t_env *env)
 {
 	if (argv && argv[0] && env)
 	{
-		if (argv[1])
+		while (*(argv += 1))
 		{
-			remove_variable_from_env(env, argv[1], GLOBAL_AND_LOCAL);
-			if (!ft_strcmp(argv[1], "PATH"))
+			remove_variable_from_env(env, *argv, GLOBAL_AND_LOCAL);
+			if (!ft_strcmp(*argv, "PATH"))
 				clear_hashtable();
 		}
 	}
--- srcs/builtin/builtin_unsetenv.c
+++ srcs/builtin/builtin_unsetenv.c
@@ -6,34 +6,36 @@
 /*   By: ble-berr <ble-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/16 08:36:15 by ble-berr          #+#    #+#             */
-/*   Updated: 2018/03/05 15:39:02 by ble-berr         ###   ########.fr       */
+/*   Updated: 2018/03/23 13:40:28 by ble-berr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 #include <unistd.h>
 
-int	builtin_unsetenv(char **argv, t_env *env)
+static int	valid_args(char **argv, t_env *env)
 {
 	if (!argv || !argv[0] || !env)
+		ft_dprintf(STDERR_FILENO, "unsetenv: internal error\n");
+	if (!argv[1])
+		ft_dprintf(STDERR_FILENO, "unsetenv: too few arguments\n");
+	else
+		return (1);
+	return (0);
+}
+
+int			builtin_unsetenv(char **argv, t_env *env)
+{
+	if (valid_args(argv, env))
 	{
-		ft_dprintf(STDERR_FILENO, "unsetenv: invalid parameters\n");
-		return (42);
-	}
-	if (argv[1])
-	{
-		if (is_valid_variable_name(argv[1]))
+		while (*(argv += 1))
 		{
-			(void)remove_variable_from_env(env, argv[1], GLOBAL);
-			if (!ft_strcmp(argv[1], "PATH"))
+			(void)remove_variable_from_env(env, *argv, GLOBAL);
+			if (!ft_strcmp(*argv, "PATH"))
 				clear_hashtable();
-			return (0);
 		}
-		else
-			ft_dprintf(STDERR_FILENO, "%s: not a valid variable name\n",
-					argv[1]);
+		return (0);
 	}
 	else
-		ft_dprintf(STDERR_FILENO, "unsetenv: too few arguments.\n");
-	return (42);
+		return (42);
 }
